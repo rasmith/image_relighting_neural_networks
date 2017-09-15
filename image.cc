91b23b545697e1e7a956cd4a4ab3e66e9b7aa646
Randall Smith <rsmith0x0@gmail.com>
2017-09-15 11:26:36 -0500

    image

---
+++ image.cc
@@ -0,0 +1,30 @@
+#include "image.h"
+
+#include <string>
+
+namespace image {
+
+Image::Image(const Image& im) : width_(im.width_), height_(im.height_) {
+  pixels_.resize(im.pixels_.size());
+  std::copy(im.pixels_.begin(), im.pixels_.end(), pixels_.begin());
+}
+
+void Image::ToPpm(std::string& result) const {
+  std::string output = "P3\n";
+  output += std::to_string(width_) + " " + std::to_string(height_) + "\n";
+  output += "255\n";
+  const Pixel* current = (const Pixel*)(&pixels_[0]);
+  for (int y = 0; y < height_; ++y) {
+    for (int x = 0; x < width_; ++x) {
+      output += std::to_string(static_cast<int>(current->r)) + " ";
+      output += std::to_string(static_cast<int>(current->g)) + " ";
+      output += std::to_string(static_cast<int>(current->b));
+      ++current;
+      if (x < width_ - 1) output += " ";
+    }
+    output += "\n";
+  }
+  result = output;
+}
+
+}  // namespace image

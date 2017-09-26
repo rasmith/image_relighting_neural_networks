881d1e67464d0217327964602fe21e088bf30e63
Randall Smith <rsmith0x0@gmail.com>
2017-09-26 17:25:14 -0500

    clustering + average image

--- fuzzy_clustering.py
+++ fuzzy_clustering.py
@@ -3,6 +3,8 @@ import glob
 import os
 import cluster
 from scipy import misc
+import matplotlib.pyplot as plt
+import numpy as np
 
 def average_image(files):
   for file in files:
@@ -28,9 +30,17 @@ width, height, channels  = image.shape
 num_hidden_nodes = 10
 
 # get average image
+print("Computing average image...")
 image_avg = average_image(files)
 
+if len(sys.argv) >=3 and sys.argv[2] == "show":
+  #show it
+  plt.figure()
+  image_plot = plt.imshow(image_avg)
+  plt.show(image_plot)
+
 # create clusters
+print("Cluster...")
 max_num_clusters =  int(\
     cluster.maximum_clusters(width, height, num_hidden_nodes, num_images))
 print("width = %d, height = %d, channels = %d, num_images = %d" % \
@@ -38,3 +48,15 @@ print("width = %d, height = %d, channels = %d, num_images = %d" % \
 pc = cluster.PixelClusters(max_num_clusters, [width, height])
 pc.cluster()
 
+if len(sys.argv) >=3 and sys.argv[2] == "show":
+  #show it
+  for i in range(0, len(pc.levels)):
+    centers, labels = pc.levels[i]
+    palette = cluster.generate_palette(len(centers[0]))
+    print("num_centers = %d" % len(centers[0]))
+    image = cluster.render_clusters(width, height, labels[0], palette)
+    image = np.array(image)
+    image = image.reshape((width, height, channels))
+    plt.figure()
+    image_plot = plt.imshow(image)
+    plt.show(image_plot)

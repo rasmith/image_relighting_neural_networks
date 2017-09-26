2a4b7504e194a7261c33d8818571690fc2c21ca5
Randall Smith <rsmith0x0@gmail.com>
2017-09-26 14:01:46 -0500

    matplotlib visualization

--- cluster.py
+++ cluster.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import colorsys
 
 import kmeans2d
 
@@ -31,9 +32,20 @@ class PixelClusters:
           for i in range(0, num_clusters)]
       labels = [cxx_labels[i] for i in range(0, width * height)]
       end = time.time()
-      num_clusters = int(num_clusters / 4)
+
+      # # plot
+      # palette = generate_palette(num_clusters)
+      # image = render_clusters(width, height, labels, palette)
+      # image = np.array(image)
+      # image = image.reshape((width, height, 3))
+      # plt.figure()
+      # image_plot = plt.imshow(image)
+      # plt.show(image_plot)
+
       self.levels.append([[centroids], [labels]])
-      print("elapsed = %s" % str(end-start))
+      elapsed = float(int((end - start)*100))/100.0
+      print("elapsed = %s" % str(elapsed))
+      num_clusters = int(num_clusters / 4)
 
 def pixels_required(num_weights, num_images):
   return 25 * num_images / num_images
@@ -47,7 +59,39 @@ def maximum_clusters(width, height, num_hidden_nodes, num_images):
   num_pixels_needed = pixels_required(num_weights, num_images)
   return num_pixels / num_pixels_needed
 
-max_num_clusters =  int(maximum_clusters(640, 480, 10, 1024))
-print("maximum_clusters = %d" % max_num_clusters)
-pc = PixelClusters(max_num_clusters, [640, 480])
-pc.cluster()
+def generate_palette(num_centers):
+  v = 0.5
+  s = 0.5
+  min_h = 0.0
+  max_h = 1.0
+  step = (max_h - min_h) / num_centers
+  h = 0.0
+  colors = [colorsys.hsv_to_rgb(step * float(i), s, v) for \
+      i in range(0, num_centers)]
+  return colors
+
+def render_clusters(width, height, labels, palette):
+  image = [palette[labels[width * y + x]]  \
+      for x in range(0, width) for y in range(0, height)]
+  return image
+
+if __name__ == "__main__":
+  import matplotlib.pyplot as plt
+  max_num_clusters =  int(maximum_clusters(640, 480, 10, 1024))
+  print("maximum_clusters = %d" % max_num_clusters)
+  width = 640
+  height = 480
+  channels = 3
+  pc = PixelClusters(max_num_clusters, [width, height])
+  pc.cluster()
+
+  for i in range(0, len(pc.levels)):
+    centers, labels = pc.levels[i]
+    palette = generate_palette(len(centers[0]))
+    print("num_centers = %d" % len(centers[0]))
+    image = render_clusters(width, height, labels[0], palette)
+    image = np.array(image)
+    image = image.reshape((width, height, channels))
+    plt.figure()
+    image_plot = plt.imshow(image)
+    plt.show(image_plot)
